ed917b1f5faff2edb8894dac4d5a5c446d2e0b12
mzdu <daniel_pub@hotmail.com>
2015-05-29 12:32:52 -0400

    updated datamodel

--- src/datamodel.py
+++ src/datamodel.py
@@ -6,16 +6,31 @@ class Message(db.Model):
     
 #     Data Model of Message is:
 #     {
-#         'url1',[
-#                 {'time1': 'msg1'},
-#                 {'time2': 'msg2'}
-#                  ],
-#         'url2',[
-#                 {'timea': 'msga'},
-#                 {'timeb': 'msgb'}
-#                 ]
+#         "Course1":
+#         {
+#             'Lecture1':[
+#                     {'time1': 'msg1'},
+#                     {'time2': 'msg2'}
+#                      ],
+#             'Lecture2':[
+#                     {'timea': 'msga'},
+#                     {'timeb': 'msgb'}
+#                     ]
+#         }
+#         "Course2":
+#         {
+#             'Lecture1':[
+#                     {'time1': 'msg1'},
+#                     {'time2': 'msg2'}
+#                      ],
+#             'Lecture2':[
+#                     {'timea': 'msga'},
+#                     {'timeb': 'msgb'}
+#                     ]
+#         }
 #     }
-
-    url = db.ListProperty()
+    courseName = db.StringProperty()
+    lectureName = db.StringProperty()
+    url = db.StringProperty()
     messageBody = db.StringProperty()
     messageDateTime = db.DateTimeProperty()
